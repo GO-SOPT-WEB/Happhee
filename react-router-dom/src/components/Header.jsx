7262b60b739f207bf9615fcfc5f3470b466c5e2f
seohee0112 <ghdtj7@naver.com>
2023-05-09 00:03:54 +0900

    feat : 공통 헤더 컴포넌트 구현

---
+++ react-router-dom/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <St.HeaderWrapper>
+      <h1>🌼해피 웹팟 추억 보관함🌼</h1>
+    </St.HeaderWrapper>
+  );
+};
+
+export default Header;
+
+const St = {
+  HeaderWrapper: styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    width: 100%;
+    padding: 2rem;
+
+    background-color: ${({ theme }) => theme.colors.Sopt_Purple};
+
+    & > h1 {
+      ${({ theme }) => theme.fonts.B_Title_1};
+      color: ${({ theme }) => theme.colors.Sopt_White};
+      cursor: pointer;
+    }
+    & > h2 {
+      padding: 1rem;
+
+      ${({ theme }) => theme.fonts.B_Title_3};
+      background-color: ${({ theme }) => theme.colors.Sopt_White};
+      border-radius: 1rem;
+
+      cursor: pointer;
+      :hover {
+        color: ${({ theme }) => theme.colors.Sopt_White};
+        background-color: ${({ theme }) => theme.colors.Sopt_Black};
+      }
+    }
+  `,
+};
