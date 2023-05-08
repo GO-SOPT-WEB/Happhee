25725be18e6133d1255bf6ba9704c40395024c2b
seohee0112 <ghdtj7@naver.com>
2023-05-09 00:04:45 +0900

    feat : 이미지카드 리스트 컴포넌트 구현

---
+++ react-router-dom/src/components/ImgCardList.jsx
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import styled from "styled-components";
+
+import ImgCard from "./ImgCard";
+const ImgCardList = () => {
+  const [imgCardList, setImgCardList] = useState();
+
+  const getImgCardList = () => {
+    fetch(`${import.meta.env.VITE_BASE_URL}/web-gallery`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.code === 200) setImgCardList(data.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    getImgCardList();
+  }, []);
+  return (
+    <St.CardListWrapper>
+      {imgCardList &&
+        imgCardList.map(({ imgSrc, id, name }) => (
+          <ImgCard key={id} imgSrc={imgSrc} id={id} name={name} />
+        ))}
+    </St.CardListWrapper>
+  );
+};
+
+export default ImgCardList;
+
+const St = {
+  CardListWrapper: styled.section`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    gap: 1rem;
+
+    width: 100%;
+    padding: 1rem;
+
+    background-color: ${({ theme }) => theme.colors.Sopt_Blue};
+  `,
+};
